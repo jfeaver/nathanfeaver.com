1d14ca7944b529f6d4acad36c065935e32c69a92
Nathan Feaver <nathan@nathanfeaver.com>
2012-10-17 17:44:44 -0600

    fixed ordinalize for numbers in teens

--- lib/Fixnum.rb
+++ lib/Fixnum.rb
@@ -1,5 +1,6 @@
 class Integer
   def ordinalize
+    return "#{self}th" if (4..20).include? self.modulo 100
     ordinal = self.to_s
     last_digit = self.modulo 10
     ordinal << case last_digit
@@ -10,3 +11,4 @@ class Integer
     end
   end
 end
+
