b5eca8d19b9171416369f1f9805817d0f9ded437
Nathan Feaver <nathan@nathanfeaver.com>
2012-10-19 03:26:21 -0600

    refactored HTMLSnippets to Helper

--- lib/functions.rb
+++ lib/functions.rb
@@ -28,36 +28,8 @@ end
 def category? item
   return (item.identifier.match(/^\/\w+\/$/) ? item : false)
 end
-=begin
-module Nanoc
-  class Item
-    def self.category?
-      self.identifier.match(/^\/\w+\/$/)
-    end
-    public :category?
-  end
-end
-=end
+
 ############### GET HTML Functions (links and such)  #####################
-
-def get_rating_image_path rating
-  path = '/images/ratings/'
-  case rating
-  when 0
-    path += 'double_thumbs_down'
-  when 1
-    path += 'thumbs_down'
-  when 2
-    path += 'thumbs_up'
-  when 3
-    path += 'double_thumbs_up'
-  end
-  path += '.gif'
-end
-
-def get_rating_image rating
-  "<img src='#{get_rating_image_path rating}' />"
-end
 
 def get_featured_image item, img_class = ''
   image_item = @items.find {|i| i.identifier == item[:featured_image]}
@@ -74,52 +46,20 @@ def get_featured_image item, img_class = ''
   tag += "'170px' />"
 end
 
-def update hash
-  template = "<h4>#{hash[:title]}</h4>\n"
-  template += "<span class='date'>Added: #{Date.parse(hash[:date]).strftime('%b. %d, %Y')}</span>\n" unless hash[:first]
-  template
-end
-
-# This function isn't used
 def render_item item
   item.reps.find {|rep| rep.name == :default }.compiled_content
 end
-
+=begin
 # This function isn't used
 def render_excerpt item, args = {}
   args[:length] ||= 54
   article = render_item item
   article.split(' ')[0..args[:length]].join(' ')
 end
-
+=end
 #### BLOG HTML FUNCTIONS ####
 
 # Spits out a Hashtag Linkable h4 element
-def blog_heading title
-  "<a name=#{title.linkify}></a><h4>#{title}</h4>"
-end
-
-def link_to_hash title, basepath = false
-  basepath ||= @item.path
-  "<a href='#{basepath}##{title.linkify}'>#{title}</a>"
-end
-
-# Links to WordPress documentation
-def link_to_wp_function text, function = false
-  unless function
-    function = text.match(/(\w+)(\(.*\))*/)[1]
-  end
-  unless function
-    raise ArgumentError, "function should not contain spaces: #{function}" if function.include? ' '
-    raise ArgumentError, "function is not recognized: #{function}"
-  end
-  "<a href='http://codex.wordpress.org/Function_Reference/#{function}', target='_blank'><code>#{text}</code></a>"
-end
-
-# Opens a new tab with the link
-def link_to_tab text, target
-  "<a href='#{target}', target='_blank'>#{text}</a>"
-end
 
 def todo message
   "<span class='todo'><span class='todo-text'>TODO:</span> #{message}</span>"
