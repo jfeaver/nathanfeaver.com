037406ed0faa43635133a17f2e43843d1a838d69
Nathan Feaver <nathan@nathanfeaver.com>
2012-10-22 21:21:09 -0600

    refactored #find and added items_of and category_of options

--- lib/classes/categories.rb
+++ lib/classes/categories.rb
@@ -5,27 +5,43 @@ module Nathan::Classes
     # the following command should return all categories that were found:
     #
     # Categories.find :all
+    #
+    # TODO: Add docs
     # 
 
-    # Eigenclasses aren't automatically initialized.  This method must be called before Category is usable
+    # Remember this must be manually called (it's a class method)
     def initialize items
       build_accessor items
       @initialized = true
     end
 
     def find args
-      raise RuntimeError, "Category lists not initialized, use Category.initialize first" unless @initialized
-      case args
-      when :all
-        return accessor.all
-      else
-        raise ArgumentError, "Unknown arguments #{args}"
-      end
+      raise RuntimeError, "Category lists not initialized, use Categories.initialize first" unless @initialized || @initializing
+
+      categories = 
+        case args
+        when :all
+          accessor.all
+        when :sidebar
+          accessor.sidebar
+        else
+          if args[:category_of]
+            item = args[:category_of]
+            accessor.category_of item
+          elsif args[:items_of]
+            #category = args[:items_of].identifier.match(/^\/(\w+)\//)[0].to_sym
+            category = args[:items_of]
+            accessor.items_of[category.to_sym]
+          else
+            raise ArgumentError, "Unknown arguments for Category.find: #{args}"
+          end
+        end
     end
 
     private
 
     def build_accessor items
+      @initializing = true
       @accessor = CategoryBuilder.new items
     end
 
