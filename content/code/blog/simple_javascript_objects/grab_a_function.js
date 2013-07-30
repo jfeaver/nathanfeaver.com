f9fb0754a07e0aeb1f2f223bfb084d9e9e23360b
jfeaver <nathan.feaver@gmail.com>
2013-07-30 10:32:12 -0600

    update the 'grab a function' example

--- content/code/blog/simple_javascript_objects/grab_a_function.js
+++ content/code/blog/simple_javascript_objects/grab_a_function.js
@@ -1,17 +1,25 @@
-// A function that can log it's own message property
-var logger = self = function() {
-  console.log(self.message);
+var Logger;
+var greeter, tayZonday, zombieKid;
+
+// A constructor that returns a function object
+function Logger(message) {
+  // set function to a variable
+  var logger = function() {
+    console.log(message);
+  };
+
+  // return the logger.  Don't use parentheses to
+  // return the function object rather than the
+  // result of the function
+  return logger
 }
-logger.message = 'hello world';
 
-// Grab some copies of the function to log different messages
-var tayZonday = logger;
-tayZonday.message = 'Chocolate Rain';
+// Each Logger returns a custom function!
+greeter   = new Logger('hello world');
+tayZonday = new Logger('Chocolate Rain');
+zombieKid = new Logger('I like turtles');
 
-var zombieKid = logger;
-zombieKid.message = 'I like turtles';
-
-logger();     // => 'hello world'
+greeter();    // => 'hello world'
 tayZonday();  // => 'Chocolate Rain'
 zombieKid();  // => 'I like turtles'
 
